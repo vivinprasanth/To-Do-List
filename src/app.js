f5065ed16122b4595aee5aa34d51d7ec5f13eb1a
vivin_prasanth <vivin.prasanth0906@gmail.com>
2024-09-16 16:34:45 +0530

    created initial setup for To-Do browser

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+const AppRoute = () => {
+    return (
+
+        <div>
+            This is To do List
+        </div>
+    )
+
+}
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("To-Do-List")) ;
+
+root.render(<AppRoute/>) ; 
