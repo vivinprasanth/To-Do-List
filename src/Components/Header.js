0d98783b227854f6a3a6e0eef7f05f3ac42f6e30
vivin_prasanth <vivin.prasanth0906@gmail.com>
2024-09-16 16:55:47 +0530

    made body and header

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return(
+
+        <div className="Header">
+            <h1>This is header.</h1>
+            
+        </div>
+    
+
+    );
+};
+
+
+export default Header ; 
--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Header from "./Components/Header";
+import Body from "./Components/Body";
 
 
 const AppRoute = () => {
     return (
-
         <div>
-            This is To do List
+            <Header />
+            <Body /> 
         </div>
-    )
-
-}
+    );
+};
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("To-Do-List")) ;
-
 root.render(<AppRoute/>) ; 
