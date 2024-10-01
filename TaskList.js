0c40d69af62a16bb6f62dc106ce2cd558a3446d1
vivin_prasanth <vivin.prasanth0906@gmail.com>
2024-10-01 08:37:15 +0530

    Getting a dummy data made, for displaying The tasks and details.

--- TaskList.js
+++ TaskList.js
@@ -2,14 +2,15 @@ const TaskList = [
         {
             "taskName" : "Task 1",
             "taskId" : 1,
-            "dueDate" : 27/01/2001,
+            "dueDate" : new Date (2002, 3, 15),
             "taskDetails" : "this is the first task from Tasklist"
         },
         {
             "taskName" : "Task 2",
             "taskId" : 2,
-            "dueDate" : 28/01/2001,
+            "dueDate" : new Date(2001, 5, 9),
             "taskDetails" : "this is the second task from Tasklist"
         }
 
 ];
+
